cdd24aa626438ee95aca93af39f9da5760657ac2
SAURABHTILAK <saurbhtilak6@gmail.com>
2025-04-29 16:13:20 +0530

    Reverse Vector using ListIterator

--- src/Vector_Programs/ReverseVectorUsingListIterator.java
+++ src/Vector_Programs/ReverseVectorUsingListIterator.java
@@ -1,5 +1,21 @@
 package Vector_Programs;
 
+import java.util.*;
 public class ReverseVectorUsingListIterator {
-
+	 public static void main(String[] args)
+	    {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter Values In Vectors");
+		Vector v=new Vector();
+		for(int i=0;i<5;i++)
+		{
+			v.add(s.nextInt());
+		}
+		ListIterator ltr=v.listIterator(v.size());
+		while(ltr.hasPrevious())
+		{
+			Object previous=ltr.previous();
+			System.out.println(previous);
+		}
+	}
 }
