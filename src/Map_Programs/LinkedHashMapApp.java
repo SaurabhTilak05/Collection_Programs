909ddc03bae5764b31da6688e331394f135fa3d9
SAURABHTILAK <saurbhtilak6@gmail.com>
2025-05-01 23:29:34 +0530

    Add data and Display data Using LinkedHashMap

---
+++ src/Map_Programs/LinkedHashMapApp.java
@@ -0,0 +1,21 @@
+package Map_Programs;
+import java.util.*;
+public class LinkedHashMapApp {
+
+	public static void main(String[] args) {
+		LinkedHashMap map =new LinkedHashMap();
+		map.put(1, "ABC");
+		map.put(2, "MNO");
+		map.put(3, "PQR");
+		map.put(4, "RST");
+		
+		
+		Set<Map.Entry> es=map.entrySet();
+			for(Map.Entry e:es)
+			{
+				System.out.println(e.getKey()+"\t"+e.getValue());
+			}
+				
+	}
+
+}
